3229a8c176f54a3dcf44053f4aa54cabf9dc5362
Athulkk <athulkkumar5@gmail.com>
2022-07-01 13:44:36 +0530

    Basic java script are finished

--- script.js
+++ script.js
@@ -10,12 +10,51 @@ function Book(name, authour, type) {
 function Display() {
 
 }
+//Prototypes
+//Implementin add function
+Display.prototype.add = function (book) {
+    // console.log(book)
+    let tableBody = document.getElementById('tablebody');
+    let elementData = `<tr>
+                        <th scope="row">1</th>
+                        <td>${book.name}</td>
+                        <td>${book.authour}</td>
+                        <td>${book.type}</td>
+                    </tr>`
+    tableBody.innerHTML += elementData;                
+}
+//Implementing clear function
+Display.prototype.clear = function(){
+    let libraryform = document.getElementById('libraryform');
+    libraryform.reset();
+
+}
 
 let libraryform = document.getElementById('libraryform');
 
 let libraryformSubmit = (e) => {
 
-    console.log('hellll')
+    let name = document.getElementById('name').value;
+    let authour = document.getElementById('authour').value;
+    let type;
+    let scifi = document.getElementById('scifi');
+    let fantasy = document.getElementById('fantasy');
+    let thriller = document.getElementById('thriller');
+
+    if (scifi.checked) {
+        type = scifi.value;
+    } else if (fantasy.checked) {
+        type = fantasy.value;
+    } else if (thriller.checked) {
+        type = thriller.value;
+    }
+
+    let book = new Book(name, authour, type);
+    console.log(book)
+    let display = new Display();
+
+    display.add(book);
+    display.clear();
     e.preventDefault();
 }
 
